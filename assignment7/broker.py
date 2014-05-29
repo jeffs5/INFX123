f391cc46d85da0e9944c6bdf95cd30d61a1a249c
jeffs5 <jeffs5s@gmail.com>
2014-05-29 13:06:53 -0700

    Assignment 7 push

---
+++ assignment7/broker.py
@@ -0,0 +1,77 @@
+from network import Listener, Handler, poll
+
+
+handlers = {}  # map client handler to user name
+names = {} # map name to handler
+subs = {} # map tag to handlers
+
+def broadcast(msg):
+    for h in handlers.keys():
+        h.do_send(msg)
+
+
+class MyHandler(Handler):
+    
+    def on_open(self):
+        handlers[self] = None
+        
+    def on_close(self):
+        name = handlers[self]
+        del handlers[self]
+        broadcast({'leave': name, 'users': handlers.values()})
+        
+    def on_msg(self, msg):
+        if 'join' in msg:
+            name = msg['join']
+            handlers[self] = name
+            names[name] = self
+            broadcast({'join': name, 'users': handlers.values()})
+
+        elif 'speak' in msg:
+            name, txt = msg['speak'], msg['txt']
+
+
+            #adds subs for tag
+            temp = txt.split()
+
+            send_to = []
+            for s in temp:
+                if '+' in s:
+                    temp_word = s.replace('+','', 1)
+                    if temp_word not in subs:
+                        subs[temp_word] = []
+                    subs[temp_word].append(self)
+                    send_to.append(self)
+
+            #removes tag
+            for s in temp:
+                if '-' in s:
+                    temp_word = s.replace('-','', 1)
+                    if temp_word in subs:
+                        for handler in subs[temp_word]:
+                            if handler == self:
+                                subs[temp_word].remove(self)
+
+            #sends to specific users based on tag
+            for s in temp:
+                if '#' in s:
+                    temp_word = s.replace('#','', 1)
+                    if temp_word in subs:
+                        for handler in subs[temp_word]:
+                            if handler not in send_to:
+                                send_to.append(handler)
+
+            for s in temp:
+                if '@' in s:
+                    temp_word = s.replace('@','',1)
+                    if temp_word in names:
+                        if names[temp_word] not in send_to:
+                            send_to.append(names[temp_word])
+
+            
+            for h in send_to:
+                h.do_send(msg)
+
+Listener(8888, MyHandler)
+while 1:
+    poll(0.05)
